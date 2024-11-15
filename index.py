238c7e8742c2b7a875550c67534bd033042231bb
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2024-11-16 00:12:28 +0530

    finish project

--- index.py
+++ index.py
@@ -1,4 +1,26 @@
 name = input("Enter your name: ")
 indexNumber = input("Enter your index: ")
+numberOfSubjects = input("Enter the number of subjects.")
+subjectMarks = []
 
-print(f"Name is {name}. You index number is {indexNumber}")
+for i in range(int(numberOfSubjects)):
+    subject = input(f"Enter the subject number {i+1}:")
+    mark = input(f"Enter {subject} marks:")
+    subjectMarks.append({ "subject": subject, "mark": mark })
+
+print("================================================================")
+print(f"{name} with index number {indexNumber}, sat for {numberOfSubjects} subject/s.")
+
+for index, subjectMark in enumerate(subjectMarks):
+    mark = int(subjectMark["mark"])
+    subject = subjectMark['subject']
+    if mark >= 90:
+        print(f"{index+1}) {subject} - A({mark})")
+    elif mark >= 80:
+        print(f"{index+1}) {subject} - B({mark})")
+    elif mark >= 70:
+        print(f"{index+1}) {subject} - C({mark})")
+    elif mark >= 60:
+        print(f"{index+1}) {subject} - S({mark})")
+    else:
+        print(f"{index+1}) {subject} - S({mark})")
